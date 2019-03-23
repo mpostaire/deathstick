41a89c6637f94f8f179ca479b392657c8496c29d
evrardco <colin.evrard@student.uclouvain.be>
2019-03-24 00:36:09 +0100

    "ajouté debug text"

--- game.py
+++ game.py
@@ -1,14 +1,47 @@
+import os
+
+import pytmx
 import cocos
-from cocos.actions import *
-from cocos.text import Label
+
 from cocos.scene import Scene
-from cocos.layer import Layer
 from cocos.director import director
 from pyglet.window import key
+from cocos.actions import *
 import math
 
 
-class HelloWorld(Layer):
+CURRENT_TMX = None
+CURRENT_WALL_ARRAY = None
+THE_ELDER_SCROLLS_MANAGER = None
+
+
+def get_path(file):
+    return os.path.join(
+        os.path.dirname(__file__),
+        file)
+
+
+def load_tmx(map_string):
+    global CURRENT_TMX
+    CURRENT_TMX = pytmx.TiledMap(map_string)
+
+
+def get_background_path():
+    global CURRENT_TMX
+    print(CURRENT_TMX.get_tile_image_by_gid(1)[0])
+    return CURRENT_TMX.get_tile_image_by_gid(1)[0]
+
+
+def load_wall_array():
+    global CURRENT_WALL_ARRAY
+    global CURRENT_TMX
+    CURRENT_WALL_ARRAY = list()
+    for object in CURRENT_TMX.objects:
+        rect = cocos.Rect(object.x, object.y, object.width, object.height)
+        CURRENT_WALL_ARRAY.append(rect)
+
+
+class HelloWorld(cocos.layer.ScrollableLayer):
     is_event_handler = True
 
     def __init__(self):
@@ -19,14 +52,33 @@ class HelloWorld(Layer):
             image="cursor.png"
         )
         self.cursor.position = 350, 350
-        self.cursor.speed = 50
+        self.cursor.speed = 200
         self.cursor.angular_speed = 150
         self.cursor.rotation = 90
+        self.background = cocos.sprite.Sprite(
+            image=get_background_path()
+        )
+        self.cursor.position = 10, director.get_window_size()[1] - 10
+        self.cursor.velocity = 0, 0
+
+        self.label = cocos.text.Label(
+            'x: y:',
+            font_name='Times New Roman',
+            font_size=12,
+            anchor_x='center', anchor_y='center'
+        )
+        self.add(self.background)
         self.add(self.cursor)
+        self.add(self.label)
 
         self.schedule(self.update)
 
     def update(self, delta):
+        global THE_ELDER_SCROLLS_MANAGER
+        THE_ELDER_SCROLLS_MANAGER.set_focus(self.cursor.position[0], self.cursor.position[1])
+        self.label.element.text = "x: {}, y: {}".format(int(self.cursor.position[0]), int(self.cursor.position[1]))
+        self.label.position = self.cursor.position[0], self.cursor.position[1] - 10
+
         for k in self.keys_pressed:
             if k == key.LEFT:
                 self.cursor.do(RotateBy(-self.cursor.angular_speed * delta, 0))
@@ -60,6 +112,10 @@ class HelloWorld(Layer):
 
 if __name__ == '__main__':
     director.init(width=800, height=600)
-    hello_layer = HelloWorld()
-    main_scene = Scene(hello_layer)
+    load_tmx("ressources/testmap/map.tmx")
+    THE_ELDER_SCROLLS_MANAGER = cocos.layer.ScrollingManager()
+
+    THE_ELDER_SCROLLS_MANAGER.add(HelloWorld())
+    THE_ELDER_SCROLLS_MANAGER.set_focus(10, director.get_window_size()[1] - 10)
+    main_scene = Scene(THE_ELDER_SCROLLS_MANAGER)
     director.run(main_scene)
