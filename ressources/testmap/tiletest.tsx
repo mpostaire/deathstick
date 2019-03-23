f321b750958c309ba101512d9ef0bc1dfede67e6
evrardco <colin.evrard@student.uclouvain.be>
2019-03-23 19:48:57 +0100

    background

---
+++ ressources/testmap/tiletest.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tiletest" tilewidth="496" tileheight="544" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="496" height="544" source="google.png"/>
+ </tile>
+</tileset>
