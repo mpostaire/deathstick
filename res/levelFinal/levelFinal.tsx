6b42244c70a63b0ff1127654036723fd07521cb9
mpostaire <maxime_postaire@hotmail.fr>
2019-03-24 13:39:29 +0100

    m'est trompé

---
+++ res/levelFinal/levelFinal.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="levelFinal" tilewidth="1280" tileheight="1024" tilecount="1" columns="1">
+ <image source="background.png" width="1280" height="1024"/>
+</tileset>
